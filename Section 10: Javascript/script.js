59e3b97576e7c6cad6e6525c0e3fd434d0e1abfc
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2022-12-17 11:38:36 +0000

    feat: edit script js linked with index HTML of section 10 folder

---
+++ Section 10: Javascript/script.js
@@ -0,0 +1,46 @@
+
+
+/***********************************************************************
+ *  FUNCTION
+ **********************************************************************/
+// 1st Way Writing a Function
+//  function sayHello() {
+//      console.log("Hello");
+//  }
+
+// sayHello();
+
+// 2nd Way Writing a Function
+// function sing(song) {
+//     console.log(song);
+// }
+
+// sing("Laa dii daa")
+
+// 3rd Way of Writing a Function
+// function multiply(a, b) {
+//     return a*b;
+// }
+
+// 4th Way of Writing a Function
+// function multiply(a,b) {
+//     if (a > 10 || b > 10){
+//         return "that's too hard"
+//     }else {
+//         return a*b;
+//     }
+// }
+
+// 5th Way of Wiritng a Function
+// function multiply(a, b) {
+//     return a*b;
+// }
+
+// var total = multiply(3,4);
+// alert(total)
+
+/***********************************************************************
+ *  Data Structures: Arrays
+ **********************************************************************/
+var list=["tiger", "cat", "panda", "bird"]
+

a8a442b2fae2680c462911c8c9fb1bbd82714148
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2022-12-17 15:23:38 +0000

    feat: add Loop section

--- Section 10: Javascript/script.js
+++ Section 10: Javascript/script.js
@@ -4,46 +4,44 @@
  *  FUNCTION
  **********************************************************************/
 // 1st Way Writing a Function
-//  function sayHello() {
-//      console.log("Hello");
-//  }
-
-// sayHello();
+function sayHello() {
+    console.log("Hello");
+}
+sayHello();
 
 // 2nd Way Writing a Function
-// function sing(song) {
-//     console.log(song);
-// }
-
-// sing("Laa dii daa")
+function sing(song) {
+    console.log(song);
+}
+sing("Laa dii daa")
 
 // 3rd Way of Writing a Function
-// function multiply(a, b) {
-//     return a*b;
-// }
+function multiply(a, b) {
+    return a * b;
+}
 
 // 4th Way of Writing a Function
-// function multiply(a,b) {
-//     if (a > 10 || b > 10){
-//         return "that's too hard"
-//     }else {
-//         return a*b;
-//     }
-// }
+function multiply(a, b) {
+    if (a > 10 || b > 10) {
+        return "that's too hard"
+    } else {
+        return a * b;
+    }
+}
 
 // 5th Way of Wiritng a Function
-// function multiply(a, b) {
-//     return a*b;
-// }
+function multiply(a, b) {
+    return a * b;
+}
 
-// var total = multiply(3,4);
-// alert(total)
+var total = multiply(3, 4);
+alert(total)
 
 /***********************************************************************
  *  Data Structures: Arrays
  **********************************************************************/
 
-// var list=["tiger", "cat", "panda", "bird"]
+var list = ["tiger", "cat", "panda", "bird"]
 
 /***********************************************************************
  *  Data Structures: Objects
@@ -124,3 +122,79 @@ var obj = {
 
 thisIsAFunction()
 obj.thisIsAMethod()
+
+/***********************************************************************
+ *  Loops
+ **********************************************************************/
+
+// Adding exclamation mark, ! at the end of the todos list.
+var todos = [
+    "clean room",
+    "learn Javascript",
+    "exercise",
+    "brushteeth",
+    "eat healthy"
+]
+
+for (var i = 0; i < todos.length; i++) {
+    todos[i] = todos[i] + "!"
+}
+
+// Delete a finished to do list. But this method will result on halway through deleted todos
+for (var i = 0; i < todos.length; i++) {
+    todos.pop()
+}
+
+// Fix method to delete todos list
+var todosLength = todos.length;
+for (var i = 0; i < todosLength; i++) {
+    todos.pop()
+}
+
+
+// While Loop Increasing counting
+var counterOne = 0;
+while (counterOne < 10) {
+    console.log(counterOne);
+    counterOne++
+}
+
+// While Loop Decreasing counting
+var counterOne = 10;
+while (counterOne > 0) {
+    console.log(counterOne);
+    counterOne--
+}
+
+// Do While Loop
+var counterTwo = 10;
+do {
+    console.log(counterTwo);
+    counterTwo--;
+} while (counterTwo > 0);
+
+// for loop vs forEach loop
+var todos = [
+    "clean room",
+    "learn Javascript",
+    "exercise",
+    "brushteeth",
+    "eat healthy"
+]
+
+var todosLength = todos.length
+for (var i = 0; i < todosLength; i++) {
+    console.log(todos[i], i)
+}
+
+// 1st Way to write forEach function
+todos.forEach(function (todo, i) {
+    console.log(todo, i)
+})
+
+// 2nd Way to write forEach function
+function logTodos(todo, i) {
+    console.log(todo, i);
+}
+
+todos.forEach(logTodos);

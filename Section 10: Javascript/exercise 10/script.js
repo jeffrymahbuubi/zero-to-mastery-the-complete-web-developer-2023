d92710752b9f12509c308180741e5b25f24b3062
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2022-12-17 13:47:39 +0000

    feat: finish exercise: object in Javascript

---
+++ Section 10: Javascript/exercise 10/script.js
@@ -0,0 +1,30 @@
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user = {
+    username: "mahbuubi0109",
+    password: "B1u2u3b4i5"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+database = [
+    {
+        username: "mahbuubi0109",
+        password: "B1u2u3b4i5",
+    }
+]
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+newsfeed = [
+    {
+        username: "mahbuubi0109",
+        timeline: "So tired from all that learning!"
+    },
+    {
+        username: "heiligkeit",
+        timeline: "Javascript is sooooo cool!"
+    },
+    {
+        username: "jeffrymahbuubi",
+        timeline: "Javascript is preeetyy cool!"
+    }
+]
